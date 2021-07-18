9dca0b3da43453783a9e363e814a49dd916a0b85
jhansi1104 <85693359+jhansi1104@users.noreply.github.com>
2021-07-18 17:40:16 +0530

    Add files via upload

---
+++ profilepage/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Mainblock from "./container";
+import "./styles.css";
+
+const App = () => <Mainblock />;
+
+export default App;
---
+++ profilepage/container.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Heading from "./heading";
+import Form from "./form";
+import Container from "react-bootstrap/Container";
+
+const Mainblock = () => (
+  <Container className="first">
+    <Heading />
+    <Form />
+  </Container>
+);
+
+export default Mainblock;
